39afedcbd1ae66798af7e30d5a50c76685fbdd41
Tomasz Knapik <u1562595@unimail.hud.ac.uk>
2017-03-19 13:42:45 +0000

    Add searching

--- src/App.js
+++ src/App.js
@@ -11,6 +11,12 @@ class App extends Component {
           <h2>CSV Browser</h2>
         </div>
         <CsvBrowser />
+        <footer>
+          <ul>
+            <li>Tomasz Knapik</li>
+            <li><a href="mailto:u1562595@unimail.hud.ac.uk">u1562595@unimail.hud.ac.uk</a></li>
+          </ul>
+        </footer>
       </div>
     );
   }
--- src/CsvBrowser.js
+++ src/CsvBrowser.js
@@ -4,6 +4,9 @@ import Baby from 'babyparse';
 import logo from './logo.svg';
 
 class CsvBrowser extends React.Component {
+  get offsetValues() {
+    return [15, 30, 60, 90];
+  }
   get numberOfColumns() {
     const entries = this.getEntriesForPage(this.state.page);
 
@@ -15,34 +18,41 @@ class CsvBrowser extends React.Component {
   }
 
   get numberOfRecords() {
-    return this.state.logEntries.length;
+    return this.state.processedLogEntries.length;
   }
 
   constructor() {
     super();
 
     this.state = {
+      changingFile: undefined,
+      changingPageNumber: '',
+      changingSearch: '',
       loading: false,
       error: undefined,
       logEntries: [],
-      offset: 30,
-      page: 10368,
+      offset: this.offsetValues[0],
+      page: 1,
+      search: '',
+      processedLogEntries: [],
       sortBy: {}
     };
 
     this.handleGoToPage = this.handleGoToPage.bind(this);
     this.handleSortByColumn = this.handleSortByColumn.bind(this);
+    this.handleSubmitPageNumber = this.handleSubmitPageNumber.bind(this);
+    this.handleSubmitFile = this.handleSubmitFile.bind(this);
+    this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
+    this.handleChangeFile = this.handleChangeFile.bind(this);
     this.handleNextPage = this.handleNextPage.bind(this);
-    this.handlePreviousPage = this.handlePreviousPage.bind(this); 
+    this.handlePreviousPage = this.handlePreviousPage.bind(this);
+    this.handleResetFilters = this.handleResetFilters.bind(this);
     this.loadCSVFile = this.loadCSVFile.bind(this);
     this.sortArray = this.sortArray.bind(this);
   }
 
-  componentDidMount() {
-    this.loadCSVFile();
-  }
-
   componentDidUpdate(prevProps, prevState) {
+    // Sorting changed
     if (
       this.state.sortBy.columnId
       && this.state.sortBy.ascending !== undefined &&
@@ -50,33 +60,51 @@ class CsvBrowser extends React.Component {
     ) {
       this.sortArray();
     }
+
+    // Searching changed
+    if (this.state.search && this.state.search !== prevState.search) {
+      this.searchArray();
+    }
+
+    // Change current
+    if (this.state.file && this.state.file !== prevState.file) {
+      this.loadCSVFile();
+    }
   }
 
   async loadCSVFile() {
     this.setState(state => ({
       error: undefined,
       loading: true,
-      page: 1
+      page: 1,
+      sortBy: {}
     }));
 
-    const response = await fetch('example.csv');
-    const csvText = await response.text();
+    const reader = new FileReader();
+   
+    reader.onload = async (event) => {
+      const csvText = event.target.result;
 
-    const result = await Baby.parse(csvText, {
-      skipEmptyLines: true,
-      worker: true,
-      error: (err) => {
-        this.setState(() => ({
-          error: JSON.stringify(err),
-          loading: false
-        }));
-      }
-    });
+      Baby.parse(csvText, {
+        skipEmptyLines: true,
+        worker: true,
+        complete: result => {
+          this.setState(state => ({
+            loading: false,
+            logEntries: result.data,
+            processedLogEntries: result.data
+          }));
+        },
+        error: err => {
+          this.setState(() => ({
+            error: JSON.stringify(err),
+            loading: false
+          }));
+        }
+      });
+    };
 
-    this.setState(state => ({
-      loading: false,
-      logEntries: result.data
-    }));
+    reader.readAsText(this.state.file);
   }
 
   getEntriesForPage(pageNumber) {
@@ -87,81 +115,130 @@ class CsvBrowser extends React.Component {
       startIndex += 1;
     }
 
-    if (endIndex > this.numberOfRecords - 1) {
-      endIndex = this.numberOfRecords - 1;
+    if (endIndex > this.numberOfRecords) {
+      endIndex = this.numberOfRecords;
     }
-    console.log(`start index: ${startIndex}, end index: ${endIndex}`);
-    const result = this.state.logEntries.slice(startIndex, endIndex);
+    const result = this.state.processedLogEntries.slice(startIndex, endIndex);
     return result;
   }
     
   render() {
-    let firstButton;
-    let lastButton;
-    let prevButton;
-    let nextButton;
-    let logEntriesList = (
-      <p>No entries...</p>
-    );
+    let content;
 
     if (this.state.loading) {
-      logEntriesList = (
+      content = (
         <div>
           <img src={logo} className="App-logo" alt="Loading CSV file..." />
           <p>Loading CSV file...</p>
         </div>
       );
+    } else if (!this.state.file) {
+      content = (
+        <div>
+          <h1>Select file</h1>
+          <p>Select the CSV file you want to open.</p>
+          <form onSubmit={this.handleSubmitFile}>
+            <input type="file" onChange={this.handleChangeFile} name="csvFile" accept="text/csv, .csv" />
+            <input className="button" type="submit" value="Load to the browser" />
+          </form>
+        </div>
+      );
     } else if (this.state.error) {
-      logEntriesList = (
+      content = (
         <div>
           <h1>Error!</h1>
           <p>{this.state.error}</p>
         </div>
       );
-    } else if (this.state.logEntries.length) {
-      firstButton = (
-        <button disabled={this.state.page === 1} onClick={() => this.handleGoToPage(1)}>First page</button>
-      );
-
-      lastButton = (
-        <button disabled={this.state.page === this.numberOfPages} onClick={() => this.handleGoToPage(this.numberOfPages)}>Last page</button>
-      );
-
-      prevButton = (
-        <button disabled={!this.previousPageExists()} onClick={this.handlePreviousPage}>Previous page</button>
-      );
-
-      nextButton = (
-        <button disabled={!this.nextPageExists()} onClick={this.handleNextPage}>Next page</button>
-      );
-
-      const summary = (
-        <div>
-          {firstButton}
-          {prevButton}
-          {nextButton}
-          {lastButton}
+    } else {
+      const controls = (
+        <div className="controlsContainer">
           <div>
-            <button onClick={() => this.handleResetSorting()}>
-              Reset sorting
+            <button className="button" onClick={() => this.loadCSVFile()}>
+              Reload file
             </button>
-            <p>Page: {this.state.page} / <button onClick={() => this.handleGoToPage(this.numberOfPages)}>{this.numberOfPages}</button></p>
-            <p>Total records: {this.numberOfRecords}</p>
-            <p>Display:</p>
-            <ul>
-              {[30, 60, 90].map((item, key) => (
+            <button className="button" onClick={this.handleResetFilters}>Reset filter/sort</button>
+            <button className="button" onClick={() => this.setState(() => ({ file: undefined, logEntries: [] }))}>
+              Unload file
+            </button>
+          </div>
+          <div>
+            <button className="button" disabled={this.state.page === 1} onClick={() => this.handleGoToPage(1)}>First page</button>
+            <button className="button" disabled={!this.previousPageExists()} onClick={this.handlePreviousPage}>Previous page</button>
+            <span>{this.state.page} / {this.numberOfPages}</span>
+            <button className="button" disabled={!this.nextPageExists()} onClick={this.handleNextPage}>Next page</button>
+            <button className="button" disabled={this.state.page === this.numberOfPages} onClick={() => this.handleGoToPage(this.numberOfPages)}>Last page</button>
+            <form onSubmit={this.handleSubmitPageNumber}>
+              <input
+                type="number"
+                name="page"
+                placeholder="Page number..."
+                onChange={(event) =>  {
+                  const target = event.target;
+                  this.setState(() => ({
+                    changingPageNumber: target.value
+                  }));
+                }}
+                value={this.state.changingPageNumber}
+              />
+              <input className="button" type="submit" name="submitPageNumber" value="Go" />
+            </form>
+          </div>
+          <div>
+            <p>Records found: {this.numberOfRecords}</p>
+            <ul className="horizontal">
+              <li>
+                <span>Display:</span>
+              </li>
+              {this.offsetValues.map((item, key) => (
                 <li key={key}>
-                  <button onClick={() => this.changeOffsetTo(item)}>{item} records</button>
+                  <button disabled={this.state.offset === item} className="button" onClick={() => this.changeOffsetTo(item)}>{item} records</button>
                 </li>
               ))}
             </ul>
           </div>
+          <div>
+            {(() => {
+              if (this.state.search) {
+                return (
+                  <p>Searching by: {this.state.search}</p>
+                );
+              }
+            })()}
+
+            <form onSubmit={this.handleSearchSubmit}>
+              <input
+                type="text"
+                name="search"
+                placeholder="Search..."
+                onChange={(event) => {
+                  const target = event.target;
+
+                  this.setState(() => ({
+                    changingSearch: target.value
+                  }));
+                }}
+                value={this.state.changingSearch}
+              />
+              <input
+                type="submit"
+                className="button"
+                name="submitSearch"
+                value="Search"
+              />
+            </form>
+          </div>
         </div>
       );
 
-      logEntriesList = (
+      let table = (
         <div>
-          {summary}
+          <p>No entries...</p>
+        </div>
+      );
+
+      if (this.state.processedLogEntries.length) {
+        table = (
           <table>
             <thead>
               <tr>
@@ -196,14 +273,23 @@ class CsvBrowser extends React.Component {
               })}
             </tbody>
           </table>
-          {summary}
+        );
+      }
+
+      content = (
+        <div>
+          {controls}
+          <div className="tableContainer">
+            {table}
+          </div>
+          {controls}
         </div>
       );
     }
 
     return (
       <div>
-        {logEntriesList}
+        {content}
       </div>  
     );
   }
@@ -233,7 +319,7 @@ class CsvBrowser extends React.Component {
   }
 
   sortArray() {
-    const logEntriesCopy = this.state.logEntries.slice();
+    const logEntriesCopy = this.state.processedLogEntries.slice();
 
     const compareValues = (val, val2) => {
       const index = this.state.sortBy.columnId;
@@ -257,9 +343,31 @@ class CsvBrowser extends React.Component {
     logEntriesCopy.sort(compareValues);
 
     this.setState(() => ({
-      logEntries: [
+      processedLogEntries: [
         ...logEntriesCopy
       ]
+    }));
+  }
+
+  searchArray() {
+    this.setState(() => ({
+      loading: true,
+      page: 1
+    }));
+
+    const pattern = new RegExp(this.state.search, 'i');
+
+    this.setState(state => ({
+      loading: false,
+      processedLogEntries: state.processedLogEntries.filter(row => {
+        for (let column of row) {
+          if (column.search(pattern) !== -1) {
+            return true;
+          }
+        }
+
+        return false;
+      })
     }));
   }
 
@@ -277,15 +385,42 @@ class CsvBrowser extends React.Component {
     return (this.state.page < this.numberOfPages);
   }
 
+  handleResetFilters() {
+    this.setState(state => ({
+      processedLogEntries: [...state.logEntries],
+      sortBy: {},
+      search: ''
+    }));    
+  }
+
+  handleChangeFile(event) {
+    const target = event.target;
+
+    this.setState(() => ({
+      changingFile: target.files[0]
+    }));
+  }
+
+  handleSubmitFile(event) {
+    event.preventDefault();
+
+    this.setState(state => ({
+      file: state.changingFile,
+      changingFile: undefined
+    }));
+  }
+
   previousPageExists() {
     return (this.state.page > 1);
   }
 
   changeOffsetTo(offset) {
+    const currentStartIndex = ((this.state.page - 1) * (this.state.offset));
+    const newPage = Math.floor(currentStartIndex / offset) + 1;
 
     this.setState(state => ({
       offset,
-      page: Math.ceil(state.page / (state.offset/offset))
+      page: newPage
     }));
   }
 
@@ -295,12 +430,33 @@ class CsvBrowser extends React.Component {
     }));
   }
 
-  handleResetSorting() {
+  handleSubmitPageNumber(event) {
+    event.preventDefault();
+
+    if (
+      this.state.changingPageNumber > this.numberOfPages
+      || this.state.changingPageNumber <= 0
+    ) {
+      alert(`${this.state.changingPageNumber} is not a valid page number.`);
+      return;
+    }
+
+    const pageNumber = parseInt(this.state.changingPageNumber, 10);
+
     this.setState(() => ({
-      sortBy: []
+      changingPageNumber: ''
     }));
 
-    this.loadCSVFile();
+    this.handleGoToPage(parseInt(pageNumber, 10));
+  }
+
+  handleSearchSubmit(event) {
+    event.preventDefault();
+
+    this.setState(state => ({
+      changingSearch: '',
+      search: state.changingSearch
+    }));
   }
 }
 
