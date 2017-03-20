0b08e5bab3ba9fa209657b5cfce76b98fcdec7f1
Tomasz Knapik <u1562595@unimail.hud.ac.uk>
2017-03-20 23:04:41 +0000

    Move some JSX templates into separate files

---
+++ src/components/Pager/Pager.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+
+export default function render() {
+  return (
+    <div>
+      <button
+        className="button"
+        disabled={this.props.page === 1}
+        onClick={() => this.handleGoToPage(1)}
+      >
+        First page
+      </button>
+      <button
+        className="button"
+        disabled={!this.previousPageExists()}
+        onClick={this.handlePreviousPage}
+      >
+        Previous page
+      </button>
+      <span>
+        {this.props.page} / {this.numberOfPages}
+      </span>
+      <button
+        className="button"
+        disabled={!this.nextPageExists()}
+        onClick={this.handleNextPage}
+      >
+        Next page
+      </button>
+      <button
+        className="button"
+        disabled={this.state.page === this.numberOfPages}
+        onClick={() => this.handleGoToPage(this.numberOfPages)}
+      >
+        Last page
+      </button>
+      <form onSubmit={this.handleSubmitPageNumber}>
+        <input
+          type="number"
+          name="page"
+          placeholder="Page number..."
+          onChange={(event) =>  {
+            const target = event.target;
+            this.setState(() => ({
+              changingPageNumber: target.value
+            }));
+          }}
+          value={this.state.changingPageNumber}
+        />
+        <input
+          className="button"
+          type="submit"
+          name="submitPageNumber"
+          value="Go"
+        />
+      </form>
+      <div>
+        <p>Records found: {this.props.numberOfRecords}</p>
+        <ul className="horizontal">
+          <li>
+            <span>Display:</span>
+          </li>
+          {this.props.offsetValues.map((item, key) => (
+            <li key={key}>
+              <button
+                disabled={this.props.offset === item}
+                className="button"
+                onClick={() => this.handleChangeOffset(item)}
+              >
+                {item} records
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
--- src/views/FileHashCheck/index.js
+++ src/views/FileHashCheck/index.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 
-import { FileChooser, LoadingIndicator } from 'components';
 import { getFileHashes } from 'utils/file';
+
 import './FileHashCheck.css';
+import template from './FileHashCheck.jsx';
 
 export default class FileHashCheck extends Component {
   constructor() {
@@ -28,70 +29,7 @@ export default class FileHashCheck extends Component {
   }
 
   render() {
-    if (this.state.loading) {
-      return <LoadingIndicator />;
-    }
-
-    return (
-      <div>
-        {(() => {
-          if (!this.state.file) {
-            return (
-               <FileChooser
-                onChangeFile={file => this.setState(() => ({ file }))}
-              />
-            );
-          }
-        })()}
-        {this.state.error && (<p>Error: {this.state.error}</p>)}
-        {this.state.file && (<h1>File: {this.state.file.name}</h1>)}
-        {(() => {
-          if (this.state.file) {
-            return (
-              <button
-                className="button"
-                onClick={() => this.setState(() => ({ error: '', file: undefined, userHashes: '', fileHash: {} }))}
-              >
-                Unload file
-              </button>
-            );
-          }
-        })()}
-        <ul>
-          {Object.keys(this.state.fileHash).map(key => (
-            <li key={key}>
-              <span className={this.isValidWithUserHash(this.state.fileHash[key]) ? 'match' : 'no-match'}>
-                {`${key}: ${this.state.fileHash[key]} `}
-                <a
-                  href={'data:text/plain;charset=utf-8,' + encodeURIComponent(this.state.fileHash[key])}
-                  download={this.state.file.name + '.' + key}
-                >
-                  [Dowload]
-                </a>
-              </span>
-            </li>
-          ))}
-        </ul>
-        {(() => {
-          if (this.state.file) {
-            return (
-              <div>
-                <p>Please input your hashes in order to see matches.</p>
-                <p>You can input multiple hashes on new lines.</p>
-                <textarea
-                  style={{ width: '80%' }}
-                  rows="20"
-                  onChange={(event) => {
-                    const target = event.target;
-                    this.setState((event) => ({ userHashes: target.value }));
-                  }}
-                />
-              </div>
-            );
-          }
-        })()}
-      </div>
-    );
+    return template.call(this);
   }
 
   /**
