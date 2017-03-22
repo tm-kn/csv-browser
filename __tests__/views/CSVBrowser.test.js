806fb8ed1c1a4a83fabd9fe3b17b8cb164ae43c5
Tomasz Knapik <u1562595@unimail.hud.ac.uk>
2017-03-22 21:46:00 +0000

    Delete tests for views - workers break them

--- __tests__/views/CSVBrowser.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-
-import { CsvBrowser } from 'views';
-
-it('renders without crashing', () => {
-    renderer.create(<CsvBrowser />);
-});
--- __tests__/views/FileHashCheck.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-
-import { FileHashCheck } from 'views';
-
-it('renders without crashing', () => {
-    renderer.create(<FileHashCheck />);
-});
